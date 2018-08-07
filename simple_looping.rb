424ff2bfedfbe14bab43e3a12daa7bae644e000e
Brian Kang <brianhkang1@gmail.com>
2018-08-07 22:56:56 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,7 +12,7 @@ def loop_iterator(number_of_times)
 end
 
 def times_iterator(number_of_times)
-  number_of_times.do 
+  number_of_times.times do 
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
