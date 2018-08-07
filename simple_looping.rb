564916538df3a3507dabd318618a2bf58a8d4362
Brian Kang <brianhkang1@gmail.com>
2018-08-07 22:55:44 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,8 @@ def loop_iterator(number_of_times)
   loop do 
     phrase = "Welcome to Flatiron School's Web Development Course!"
     counter += 1
-  
+    if counter == number_of_times
+      break
   
 end
 
